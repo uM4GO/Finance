30ad53cf3d6b9528931b5fc738a160c1b835f8d8
Gabriel Faustini <gabrielfaustini9@gmail.com>
2026-05-15 18:31:27 -0300

    Tabela clientes criada

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_06_171021) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_14_174120) do
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
     t.string "agency_number"
@@ -24,6 +24,18 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_06_171021) do
     t.boolean "active", default: true
     t.string "code", limit: 3
     t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.boolean "active"
+    t.text "address"
+    t.date "birth_date"
+    t.string "cep", limit: 9
+    t.string "cpf", limit: 14
+    t.datetime "created_at", null: false
+    t.string "kind", limit: 1
     t.string "name"
     t.datetime "updated_at", null: false
   end
