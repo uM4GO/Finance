799f940940686b2d5dc4a28543d672b68956daf2
Gabriel Faustini <gabrielfaustini9@gmail.com>
2026-05-29 08:09:58 -0300

    alterações clientes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,26 @@
+
 class CustomersController < ApplicationController
+  before_action :set_customer, only: %i[ edit update destroy ]
+
   def index
     @customers = Customer.all
   end
 
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,30 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :address, presence: true
+  validate :valid_cpf?
+  
+  enum :kind, { fisica: "F", juridica: "J" }
+
+  private
+  
+  def valid_cpf?
+    #if kind == "J"
+    #  return true
+    #elsif kind == "F"
+     # if cpf.nil?
+      #  errors.add(:cpf, "CPF não pode ser menor nulo.")
+       # return false
+      #end
+      #if cpf.size < 13
+       # errors.add(:cpf, "CPF não pode ser menor que 13 caracteres.")
+      #end
+    #end
+    return true if juridica?
+    #if cpf.nil?
+      errors.add(:cpf, "Não pode ser nulo.") if cpf.nil?
+      #return false
+    #end
+    errors.add(:cpf, "CPF não pode ser menor que 13 caracteres.") if cpf.size < 13
+
+  end
 end
