c519e1a795c82f6a035a6e33d34e01594ee8393b
Gabriel Faustini <gabrielfaustini9@gmail.com>
2026-06-07 20:07:12 -0300

    Mudanças bootstrap

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,8 @@
 module CustomersHelper
+	def customer_kind_options
+		Customer.kinds.map do |key, _value|
+			translation_text = t("activerecord.attributes.customer.enums.kind.#{key}")	
+			[translation_text, key]
+		end
+	end
 end
