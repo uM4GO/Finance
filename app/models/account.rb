eeaa80a94d4fcd5788511088991ecc8b661fea3f
Gabriel Faustini <gabrielfaustini9@gmail.com>
2026-06-22 14:07:15 -0300

    Botão excluir só para contas sem transações

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :bank
-  has_many :transactions
+  has_many :transactions, dependent: :restrict_with_exception
 
   validates :agency_number, presence: true  
   validates :account_number, presence: true, uniqueness: { scope: [:agency_number, :bank_id] }
